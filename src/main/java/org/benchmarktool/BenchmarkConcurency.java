91274ff82c6b82db9d687a461b39d6a4a9dca835
ramil-fatykhov <ramikr@live.ru>
2020-01-30 00:02:52 +0300

    Added concurency (in progress)

---
+++ src/main/java/org/benchmarktool/BenchmarkConcurency.java
@@ -0,0 +1,57 @@
+package org.benchmarktool;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class BenchmarkConcurency {
+    private final List<Long> timers = Collections.synchronizedList(new ArrayList<>());
+    private final AtomicInteger successes = new AtomicInteger();
+    private final AtomicInteger fails = new AtomicInteger();
+    private final AtomicLong totalBytesCount = new AtomicLong();
+
+    void addTimer(long time) {
+        timers.add(time);
+    }
+
+    void incrementFails() {
+        fails.incrementAndGet();
+    }
+
+    void incrementSuccesses() {
+        successes.incrementAndGet();
+    }
+
+    public int getFails() {
+        return fails.get();
+    }
+
+    public int getSuccesses() {
+        return successes.get();
+    }
+
+    public long getTimersSum() {
+        return timers.stream().mapToLong(Long::longValue).sum();
+    }
+
+    public void addBytes(long count) {
+        totalBytesCount.addAndGet(count);
+    }
+
+    public long getTotalBytesCount() {
+        return totalBytesCount.get();
+    }
+
+    public OptionalDouble getTimersAverage() {
+        return timers.stream().mapToLong(Long::longValue).average();
+    }
+
+    public long getPercentileBy(double percentile) {
+        timers.sort(Comparator.comparingLong(Long::longValue));
+        int index = (int) Math.ceil((percentile / (double) 100) * (double) timers.size());
+        if (index == -1) {
+            return 0;
+        }
+        return timers.get(index - 1);
+    }
+}
